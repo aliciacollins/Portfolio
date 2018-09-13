6bfe4610e32f425ddfec356dbf268d8c2d5f5974
Alicia Collins <alicia.collins60@me.com>
2018-09-13 11:40:42 -0700

    updated resume link

--- src/components/footer.js
+++ src/components/footer.js
@@ -18,7 +18,7 @@ export default props =>{
                     <a className="" target="_self" href="https://github.com/aliciacollins"><img id="github-logo" src={githubLogo} alt="Github" /><br/>Github</a>
                   </div>
                 <div className="footer-links">
-                    <a className="" target="_self" href="https://drive.google.com/file/d/1l2ahY-nF_kRx3kXDh9Sa0u3YXUUrddWw/view?usp=sharing"><i className="medium material-icons">description</i><br/>Resume</a>
+                    <a className="" target="_self" href="https://docs.google.com/document/d/1EiIdaGvLc65Hl_aZOB_TZqeHIT8xo3Xk9A1ZZQeB4g0/edit?usp=sharing"><i className="medium material-icons">description</i><br/>Resume</a>
                 </div>
                 {/* <div className="footer-links">
                     <a target="_blank" href="www.linkedin.com/in/alicia-collins-is-linked"><img id="linkedin-logo" src={linkedinLogo} alt="LinkedIn" /><br/>LinkedIn</a>
